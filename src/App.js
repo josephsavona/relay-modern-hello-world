cf949fc163891e0b58c366658c948b5c14d456a6
Joseph Savona <josephsavona@users.noreply.github.com>
2017-04-19 09:28:58 -0700

    Use a fragment container to encourage colocation
    Since the rendering logic of the feed is broken out into a separate component, this is a good chance to demonstrate colocation via a fragment container.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import {
   QueryRenderer,
+  createFragmentContainer,
   graphql,
 } from 'react-relay';
 
@@ -18,13 +19,8 @@ class App extends Component {
           query={graphql`
             query AppFeedQuery {
               feed (type: NEW, limit: 5) {
-                repository {
-                  owner { login }
-                  name
-                  stargazers_count
-                }
-
-                postedBy { login }
+                id
+                ...FeedEntry
               }
             }
           `}
@@ -34,7 +30,15 @@ class App extends Component {
               return <div>{error.message}</div>;
             } else if (props) {
               console.log(props.feed);
-              return <Feed feed={props.feed} />;
+              return (
+                <ol>
+                  {feed.map(entry => (
+                    <li key={entry.id}>
+                      <FeedEntry data={entry} />
+                    </li>
+                  ))}
+                </ol>
+              );
             }
             return <div>Loading</div>;
           }}
@@ -49,14 +53,23 @@ class App extends Component {
   }
 }
 
-const Feed = ({ feed }) => (
-  <ol>
-    {feed.map(entry => (
-      <li key={entry.repository.owner.login + '/' + entry.repository.name}>
-        {entry.repository.owner.login}/{entry.repository.name}: {entry.repository.stargazers_count} Stars
-      </li>
-    ))}
-  </ol>
-)
+const FeedEntry = createFragmentContainer(
+  ({ data }) => (
+    <div>
+      <div>{entry.repository.owner.login}/{entry.repository.name}: {entry.repository.stargazers_count} Stars</div>
+      <div>Posted by {entry.postedBy.login}</div>
+    </div>
+  ),
+  graphql`
+    fragment FeedEntry on Entry {
+      repository {
+        owner { login }
+        name
+        stargazers_count
+      }
+      postedBy { login }
+    }
+  `
+);
 
 export default App;
